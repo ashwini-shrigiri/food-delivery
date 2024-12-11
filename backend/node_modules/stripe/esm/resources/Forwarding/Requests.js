2ce2ef591167aee6ee2f068d599a6a2d16dec474
ashwini_shrigiri <ashwinishrigiri392@gmail.com>
2024-12-11 12:10:11 +0530

    Add files via upload

--- backend/node_modules/stripe/esm/resources/Forwarding/Requests.js
+++ backend/node_modules/stripe/esm/resources/Forwarding/Requests.js
@@ -1,10 +1,7 @@
-"use strict";
 // File generated from our OpenAPI spec
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Requests = void 0;
-const StripeResource_js_1 = require("../../StripeResource.js");
-const stripeMethod = StripeResource_js_1.StripeResource.method;
-exports.Requests = StripeResource_js_1.StripeResource.extend({
+import { StripeResource } from '../../StripeResource.js';
+const stripeMethod = StripeResource.method;
+export const Requests = StripeResource.extend({
     create: stripeMethod({ method: 'POST', fullPath: '/v1/forwarding/requests' }),
     retrieve: stripeMethod({
         method: 'GET',
--- backend/node_modules/stripe/esm/resources/Identity/VerificationReports.js
+++ backend/node_modules/stripe/esm/resources/Identity/VerificationReports.js
@@ -1,10 +1,7 @@
-"use strict";
 // File generated from our OpenAPI spec
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VerificationReports = void 0;
-const StripeResource_js_1 = require("../../StripeResource.js");
-const stripeMethod = StripeResource_js_1.StripeResource.method;
-exports.VerificationReports = StripeResource_js_1.StripeResource.extend({
+import { StripeResource } from '../../StripeResource.js';
+const stripeMethod = StripeResource.method;
+export const VerificationReports = StripeResource.extend({
     retrieve: stripeMethod({
         method: 'GET',
         fullPath: '/v1/identity/verification_reports/{report}',
