89f80a14b581669906853ff4a551e8424eba325f
ashwini_shrigiri <ashwinishrigiri392@gmail.com>
2024-12-11 16:18:20 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-fhut.onrender.com'
+export const url = 'https://food-delivery-backend-zy8q.onrender.com'
