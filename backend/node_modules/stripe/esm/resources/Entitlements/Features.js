de75dad84cec3db8dca2f6f8efbbf0750c3db790
ashwini_shrigiri <ashwinishrigiri392@gmail.com>
2024-12-11 12:09:36 +0530

    Add files via upload

--- backend/node_modules/stripe/esm/resources/Entitlements/Features.js
+++ backend/node_modules/stripe/esm/resources/Entitlements/Features.js
@@ -1,10 +1,7 @@
-"use strict";
 // File generated from our OpenAPI spec
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Features = void 0;
-const StripeResource_js_1 = require("../../StripeResource.js");
-const stripeMethod = StripeResource_js_1.StripeResource.method;
-exports.Features = StripeResource_js_1.StripeResource.extend({
+import { StripeResource } from '../../StripeResource.js';
+const stripeMethod = StripeResource.method;
+export const Features = StripeResource.extend({
     create: stripeMethod({ method: 'POST', fullPath: '/v1/entitlements/features' }),
     retrieve: stripeMethod({
         method: 'GET',
